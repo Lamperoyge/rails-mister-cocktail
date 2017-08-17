6a861aa8b9b7dc694598356f9da0e3bb01597de9
Adrian Mustea <mustea.adrian95@gmail.com>
2017-08-17 18:24:02 +0100

    works

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,37 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+    @cocktail.doses.build
+  end
+
+  def create
+    @cocktail = Cocktail.create(cocktail_params)
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    @dose.save
+
+    if @cocktail && @dose
+      redirect_to cocktails_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+  def dose_params
+    params.require(:cocktail).permit(doses_attributes: [:description, :ingredient_id])[:doses_attributes]["0"]
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "json"
+require "open-uri"
+text = open("http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
+ingredients = JSON.parse(text)
+
+
+ingredients["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
+
+
