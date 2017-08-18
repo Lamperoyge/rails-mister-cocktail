7c9e23bb58fc13698e47f8a3d4b5d335d24f4c0d
Adrian Mustea <mustea.adrian95@gmail.com>
2017-08-18 17:28:05 +0100

    added reviews

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,7 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+  has_many :reviews, dependent: :destroy
   validates :name, uniqueness: true, presence: true
   accepts_nested_attributes_for :doses
   mount_uploader :photo, PhotoUploader
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'cocktails#index'
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create, :destroy]
+    resources :reviews, only: [:create]
   end
 end
